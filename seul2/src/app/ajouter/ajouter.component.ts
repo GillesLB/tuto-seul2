6e0b222533281d7ab4a7fa7a81f06d6dccb0054f
GillesLB <deltag@laposte.net>
2019-08-18 16:14:43 +0200

    Ajouter ok

--- seul2/src/app/ajouter/ajouter.component.ts
+++ seul2/src/app/ajouter/ajouter.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Truc } from 'src/app/modeles/truc.modele';
+import { TrucServiceService } from 'src/app/services/truc-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ajouter',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjouterComponent implements OnInit {
 
-  constructor() { }
+  trucForm: FormGroup;
+
+  listeTrucs: Truc[] = [];
+
+  constructor(
+    private trucService: TrucServiceService,
+    private router: Router,
+  ) {
+    this.listeTrucs = this.trucService.listeTrucs;
+  }
 
   ngOnInit() {
+    this.trucForm = new FormGroup({
+      nom: new FormControl('', Validators.required),
+      couleur: new FormControl('', Validators.required),
+      prix: new FormControl('', Validators.required),
+    });
+  }
+
+  idMax() {
+    const idMaxStock = [];
+    for (const idATrier of this.listeTrucs) {
+      idMaxStock.push(idATrier.id);
+    }
+    console.log('!! : ', idMaxStock);
+    const idMax = Math.max(...idMaxStock);
+    return idMax;
+  }
+
+  fixerStock() {
+    const nouveauStock = Math.floor(Math.random() * 10);
+    return nouveauStock;
+  }
+
+  ajouterTruc() {
+    const id =  this.idMax() + 1;
+    const nom = this.trucForm.get('nom').value;
+    const couleur = this.trucForm.get('couleur').value;
+    const prix = this.trucForm.get('prix').value;
+    const stock = this.fixerStock();
+    const trucAAjouter = new Truc(id, nom, couleur, prix, stock);
+    this.listeTrucs.push(trucAAjouter);
+    this.router.navigate(['liste']);
   }
 
 }
--- seul2/src/app/app.module.ts
+++ seul2/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,9 @@ import { TrucServiceService } from 'src/app/services/truc-service.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     TrucServiceService,
